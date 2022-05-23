3790eb0526ef77dbf0c98958c0c5b5a191361ffe
renzhezhilu <renzhezhilu@yahoo.com>
2022-05-23 15:09:40 +0800

    update

--- demo/service-worker.js
+++ demo/service-worker.js
@@ -16,7 +16,7 @@
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
 const PRECACHE = 'precache-v1';
-const RUNTIME = 'runtime';
+const RUNTIME = '0.0.1';
 
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
