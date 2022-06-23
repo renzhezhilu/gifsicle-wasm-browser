b0e5a5bc99c41b8960f253fecc1fcd87232eb367
renzhezhilu <renzhezhilu@yahoo.com>
2022-06-24 05:54:43 +0800

    update

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -3,7 +3,7 @@
 // Names of the two caches used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
-const PRECACHE = "1.5.14";
+const PRECACHE = "1.5.15";
 const RUNTIME = "runtime";
 
 // A list of local resources we always want to be cached.
